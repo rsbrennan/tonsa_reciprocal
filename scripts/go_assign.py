83734d534cb49a21447595383509c8852d71ca09
Reid S Brennan <rbrennan@pespenilab.uvm.edu>
2018-10-29 14:13:34 -0400

    add dge and snp to go asign

--- scripts/go_assign.py
+++ scripts/go_assign.py
@@ -14,11 +14,13 @@ filepath = '/data/atonsa/Atonsa_gen_trans_agp_gff/blast2go_gff_export_20170613_0
 ####
 
 #make empty array
-go_df = np.empty(shape=(7898,2), dtype = object)
+num_lines = sum(1 for line in open('/users/r/b/rbrennan/reciprocal_t/analysis/snp_loadings_go.txt'))-1
+
+go_df = np.empty(shape=(num_lines,2), dtype = object)
 
 i=0 # start counter
 
-with open('/users/r/b/rbrennan/reciprocal_t/analysis/heterozygosity_output.txt') as master_file:
+with open('/users/r/b/rbrennan/reciprocal_t/analysis/snp_loadings_go.txt') as master_file:
         head1 = next(master_file)
         for idx, line in enumerate(master_file):
             tmp_gene = line.split("\t")[0]
@@ -36,14 +38,57 @@ with open('/users/r/b/rbrennan/reciprocal_t/analysis/heterozygosity_output.txt')
                             if len(out_go) == 0:
                                 out_go = go_tmp
                             if len(out_go) > 0:
-                                out_go = go_tmp + "," + go_tmp
+                                out_go = out_go + "," + go_tmp
             go_df[idx,0] = tmp_gene
             if len(out_go) > 0:
-                go_df[idx,1] = out_go.replace('"',"").replace(',',";")
+                out_go = ";".join(set(out_go.split(","))) # remove duplicates, join together with string.
+                go_df[idx,1] = out_go
+                #go_df[idx,1] = out_go.replace('"',"").replace(',',";")
             if len(out_go) == 0:
-                go_df[idx,1] = "NA"
+                go_df[idx,1] = "unknown"
             i=i+1
             if i % 500 == 0: print(i)
 
-np.savetxt('/users/r/b/rbrennan/reciprocal_t/analysis/heterozygosity_GOterms.out', go_df,fmt='%5s', delimiter='\t')
 
+np.savetxt('/users/r/b/rbrennan/reciprocal_t/analysis/GO_enrich/snp_GOterms.out', go_df,fmt='%s', delimiter='\t')
+
+
+## dge loadings
+
+#make empty array
+num_lines = sum(1 for line in open('/users/r/b/rbrennan/reciprocal_t/analysis/dge_loadings.txt'))-1
+
+go_df = np.empty(shape=(num_lines,2), dtype = object)
+
+i=0 # start counter
+
+with open('/users/r/b/rbrennan/reciprocal_t/analysis/snp_loadings_go.txt') as master_file:
+        head1 = next(master_file)
+        for idx, line in enumerate(master_file):
+            tmp_gene = line.split("\t")[0]
+            out_go = []
+            # match GO terms
+            match = []
+            with gzip.open(filepath, mode="rt") as file:
+                for gene_line in file:
+                    if len(re.findall(tmp_gene, gene_line)) > 0:
+                        gene_spl = gene_line.split("\n")[0].split("\t")
+                        go_spl = [i for i in gene_spl[8].split(";") if i.startswith('Ontology_id')]
+                        if len(go_spl) > 0:
+                            if len(go_spl[0].split()) > 1:
+                                go_tmp = go_spl[0].split()[1]
+                            if len(out_go) == 0:
+                                out_go = go_tmp
+                            if len(out_go) > 0:
+                                out_go = out_go + "," + go_tmp
+            go_df[idx,0] = tmp_gene
+            if len(out_go) > 0:
+                out_go = ";".join(set(out_go.split(",")))
+                go_df[idx,1] = out_go
+                #go_df[idx,1] = out_go.replace('"',"").replace(',',";")
+            if len(out_go) == 0:
+                go_df[idx,1] = "unknown"
+            i=i+1
+            if i % 500 == 0: print(i)
+
+np.savetxt('/users/r/b/rbrennan/reciprocal_t/analysis/GO_enrich/dge_GOterms.out', go_df,fmt='%s', delimiter='\t')
