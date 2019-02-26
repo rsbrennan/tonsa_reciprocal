cfc2def9ba7bab46c8c7a834618a76bb2efd63be
Reid S Brennan <rbrennan@pespenilab.uvm.edu>
2019-02-26 16:26:27 -0500

    organizing

--- scripts/salmon_index.sh
+++
@@ -1,5 +0,0 @@
-# will use the quasi mapping approach- it is faster than the original. It is actually unnecessary to specify as it is the default
-# here, k is the minimum acceptable length for a valid match. This works well with 75 bp reads or longer
-
-salmon index -t /data/atonsa/Atonsa_gen_trans_agp_gff/trinity.trimmomatic.above500.noPhiX.fasta.gz \
-    -i /data/atonsa/Atonsa_gen_trans_agp_gff/tonsa_index --type quasi -k 31
